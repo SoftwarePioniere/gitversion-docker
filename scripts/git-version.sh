80473433d3917431524143191daf0f0d63ff3ec5
Tobias Böker <tobias.boeker@outlook.com>
2017-04-28 11:54:01 +0200

    ...

--- scripts/git-version.sh
+++ scripts/git-version.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-echo ":: Test if GitVersion.yml file exist in Repository"
-#test -f /src/GitVersion.yml
-#echo ":: Result:" 
-#echo $?
-
-#if [ $? -eq 0 ]
-#then
-#	echo ":: GitVersion.yml file doesn't exist in Repository"
-#	echo ":: copy yml file to folder"
-	#cp /usr/lib/GitVersion.yml /src/GitVersion.yml	
-# fi
-
 echo  ":: run gitversion"
 exec mono /usr/lib/GitVersion/tools/GitVersion.exe "$@"
--- scripts/vsts.sh
+++ scripts/vsts.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-function jsonval {
-    temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop`
-    echo ${temp##*|}
-}
 echo ":: running vsts script"
 echo ":: current dir:"$PWD
-#ls -a
-
-#echo ":: changing dir to /src"
-#cd /src
-#ls -a
 
 echo  ":: running gitversion"
 mono /usr/lib/GitVersion/tools/GitVersion.exe > GitVersion.json
@@ -21,7 +12,6 @@ echo $json
 
 echo ":: reading keys from json file"
 arr=( $(jq 'keys[]' GitVersion.json) )
-#printf "%s\n" ${arr[@]}
 
 echo ":: iterating keys"
 for i in "${arr[@]}"
